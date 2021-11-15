f4717d745bb4dff2ccd654992fd7a2eb0f5c7530
Jack Allen <jack.allen.web@gmail.com>
2021-11-15 17:00:13 +0000

    Added Spring Boot initial files

---
+++ src/main/java/com/jacksapps/cookingscrapbook/CookingScrapbookApplication.java
@@ -0,0 +1,13 @@
+package com.jacksapps.cookingscrapbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingScrapbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingScrapbookApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jacksapps/cookingscrapbook/HomeController.java
@@ -0,0 +1,13 @@
+package com.jacksapps.cookingscrapbook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "Home page";
+    }
+}
